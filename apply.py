ff95ad2d9db1e705f2668563e7ff5f89d58a5815
Francisco Arevalo <farevalod@gmail.com>
2016-08-23 11:39:06 -0300

    Cambios payload json

--- apply.py
+++ apply.py
@@ -3,15 +3,15 @@ import requests
 url = "http://platan.us/jobs/apply"
 content_type = "application/json"
 payload = {
-    "name": "Francisco Arevalo",
-    "email": "farevalod@gmail.com",
-    "github": "http://github.com/farevalod",
-    "resume": "http://desastre.cl/cv.txt",
-    "city": "Santiago",
+    "name":"Francisco Arevalo",
+    "email":"farevalod@gmail.com",
+    "github":"http://github.com/farevalod",
+    "city":"Santiago",
     "other": {
-        "notes":"Hice un mini script usando la lib requests de python, y 'nc -l' para levantar un server e ir revisando los POST que genera",
+        "resume":"http://desastre.cl/cv.txt",
+        "notes":"Hice un mini script usando la lib requests de python, y nc -l para levantar un server e ir revisando los POST que genera",
         "script_src":"https://github.com/farevalod/platanus_apply",
-        "interests": "Skiing, Mountaineering, Rock Climbing, Cooking, Graphic Design, Typography",
+        "interests":"Skiing, Mountaineering, Rock Climbing, Cooking, Graphic Design, Typography",
         "BTC Wallet":"12sPLuqb52q4B9JEgfENpuG1mLAjp9REYj"
     }
 }
